02d42b512e0ffb014a65bf51c67e20314bec3b6f
Thomasogwuche <thomasogwuche88@gmail.com>
2023-02-16 11:04:53 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ *main - Entry Point of the function a line with puts
+ *
+ *Return: 0 for succesful exit
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
