aa1ee4a2a1d6d5c0055db9da7fc31b386bb27e47
Thomasogwuche <thomasogwuche88@gmail.com>
2023-03-02 23:30:20 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,21 +1,27 @@
 #include "main.h"
 
 /**
- * rev_string - reverse array
- * @n: integer params
- * Return: 0
+ * infinite_add - Adds two numbers represented as strings and stores the result
+ *                in a buffer.
+ *
+ * @n1: The first number as a string.
+ * @n2: The second number as a string.
+ * @r: The buffer to store the result.
+ * @size_r: The size of the buffer r.
+ *
+ * Return: A pointer to the result string on success, or 0 on failure.
  */
-#include <string.h>
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r) {
-    int len1 = strlen(n1);
-    int len2 = strlen(n2);
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+    int len1 = _strlen(n1);
+    int len2 = _strlen(n2);
     int carry = 0, sum = 0;
     int i = len1 - 1, j = len2 - 1, k = size_r - 1;
 
-    r[k] = '\0'; // set the terminating character
+    r[k] = '\0'; /* Set the terminating character */
 
-    while (i >= 0 || j >= 0) {
+    while (i >= 0 || j >= 0)
+    {
         int d1 = (i >= 0) ? n1[i--] - '0' : 0;
         int d2 = (j >= 0) ? n2[j--] - '0' : 0;
 
@@ -23,19 +29,40 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r) {
         carry = sum / 10;
         sum = sum % 10;
 
-        if (k < 1) {
-            return 0; // result can not be stored in r
+        if (k < 1)
+        {
+            return (0); /* Result can not be stored in r */
         }
         r[--k] = sum + '0';
     }
 
-    if (carry > 0) {
-        if (k < 1) {
-            return 0; // result can not be stored in r
+    if (carry > 0)
+    {
+        if (k < 1)
+        {
+            return (0); /* Result can not be stored in r */
         }
         r[--k] = carry + '0';
     }
 
-    return &r[k];
+    return (&r[k]);
 }
 
+/**
+ * _strlen - Calculates the length of a string.
+ *
+ * @s: The string to calculate the length of.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+    int len = 0;
+
+    while (s[len] != '\0')
+    {
+        len++;
+    }
+
+    return (len);
+}
