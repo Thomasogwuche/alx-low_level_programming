bd071e3f9999ea8cb699ecc0e312fc59edaf1181
Thomasogwuche <thomasogwuche88@gmail.com>
2023-04-10 15:04:34 -0700

    quick fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,46 +1,19 @@
 #include "main.h"
 /**
- * _power - calculate (base and power)
- * @base: base of the exponet
- * @pow: power of the exponet
- * Return: value of base and power
- */
-unsigned long int _power(unsigned int base, unsigned int pow)
-{
-	unsigned long int num;
-	unsigned int i;
-
-	num = 1;
-	for (i = 1; i <= pow; i++)
-		num *= base;
-	return (num);
-}
-/**
- * print_binary - prints the binary representation of a number
- * @n: num of prented
- * Return: void
- */
+  * print_binary - converts a decimal number to binary
+  * @n: integer to convert
+  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int dev, result;
-	char flag;
+	int i, flag;
 
-	flag = 0;
-	dev = _power(2, sizeof(unsigned long int) * 8 - 1);
-
-	while (dev != 0)
+	if (n == 0)
+		_putchar('0');
+	for (flag = 0, i = sizeof(n) * 8 - 1; i >= 0; i--)
 	{
-		result = n & dev;
-		if (result == dev)
-		{
-			flag = 1;
-			_putchar('1');
-
-		}
-		else if (flag == 1 || dev == 1)
-		{
-			_putchar('0');
-		}
-		dev >>= 1;
+		if ((n >> i) & 1)
+		flag = 1;
+		if (flag == 1)
+			((n >> i) & 1) ? _putchar('1') : _putchar('0');
 	}
 }
